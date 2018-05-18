cdc48d0a5e4aaf8835cfbcc882266e192dbc3265
shengliangd <shengliangd@outlook.com>
2018-05-18 15:09:12 +0800

    add code skeleton for cunik, remove useless code

--- api/models/cunik/cunik.py
+++ api/models/cunik/cunik.py
@@ -1,5 +1,37 @@
 """class Cunik."""
 
 
+class CunikConfig:
+    """Config of a Cunik, constructed when the user wants to create a Cunik."""
+    def __init__(self):
+        pass
+
+
 class Cunik:
-    pass
+    """Represent an existing Cunik."""
+    @staticmethod
+    def create(config: CunikConfig):
+        """Create a Cunik according to the config."""
+        # Deal with the config using unikernel backends
+        # Create virtual machine using vm backends
+        # Register in the Cunik registry
+        pass
+
+    @staticmethod
+    def destroy(cunik: Cunik):
+        """Destroy a Cunik according to the config."""
+        # Stop the vm
+        # Delete it from Cunik registry.
+        pass
+
+    def __init__(self):
+        """Take configuration and create a Cunik(vm with unikernel)."""
+        pass
+
+    def start(self):
+        """Start the Cunik."""
+        pass
+
+    def stop(self):
+        """Stop the Cunik."""
+        pass
--- backend/vm/__init__.py
+++ backend/vm/__init__.py
@@ -2,13 +2,3 @@
 
 import libvirt
 import sys
-
-
-class Config:
-    """Unified configuration for all backends."""
-    pass
-
-
-def create(conf: Config):
-    """Launch a vm specified by conf."""
-    pass
