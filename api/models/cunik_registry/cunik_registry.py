b4bc4cf00ecbd8010cd6436a85c41e5b0b71716e
Mingliang Zeng <zengmingliang1998@gmail.com>
2018-06-08 21:15:45 +0800

    fix

--- api/models/cunik_registry/cunik_registry.py
+++ api/models/cunik_registry/cunik_registry.py
@@ -7,17 +7,17 @@ class CunikRegistry:
     def __init__(self):
         self._cuniks = {}
 
-    def register(self, uuid, cunik):
-        assert not self._cuniks.get(uuid)
-        self._cuniks[uuid] = cunik
+    def register(self, cunik):
+        assert not self.query(cunik.id)
+        self._cuniks[cunik.id] = cunik
 
-    def query(self, uuid):
-        return self._cuniks.get(uuid)
+    def query(self, id):
+        return self._cuniks.get(id)
 
-    def remove(self, uuid):
-        assert self.query(uuid)
-        self._cuniks[uuid] = None
+    def remove(self, cunik):
+        assert self.query(cunik.id)
+        self._cuniks[cunik.id].pop(cunik.id)
 
-    def populate(self, uuid, cunik):
-        assert self.query(uuid)
-        self._cuniks[uuid] = cunik
+    def populate(self, cunik):
+        assert self.query(cunik.id)
+        self._cuniks[cunik.id] = cunik
