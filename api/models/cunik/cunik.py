9826e0361a43d31ddbdf576e94f1e7c7c99ed042
Mingliang Zeng <zengmingliang1998@gmail.com>
2018-06-08 21:15:26 +0800

    Cunik

--- api/models/cunik/cunik.py
+++ api/models/cunik/cunik.py
@@ -1,15 +1,19 @@
 """class Cunik."""
 
 from api.models.image import ImageRegistry
-from backend.vm import VM
+from api.models.cunik_registry import CunikRegistry
+from backend.vm import VM, VMConfig
+import uuid
 
 
 class CunikConfig:
     """Config of a cunik, constructed when the user wants to create a Cunik."""
     def __init__(self, **kwargs):
-        self.name = kwargs['name']
-        self.image = kwargs['image']
-        self.cmd = kwargs['cmd']
+        self.name = kwargs['name'] # name of Cunik instance
+        self.img = kwargs['img'] # path to image file
+        self.cmd = kwargs['cmd'] # command line parameters
+        self.vmm = kwargs['vmm'] # VM type
+        self.mem = kwargs['mem'] # memory size in KB
 
 
 class Cunik:
@@ -25,24 +29,40 @@ class Cunik:
         >>> del cu  # NOTICE: This really destroys corresponding vm and remove this cunik from registry
     """
     def __init__(self, config: CunikConfig):
+        """Initialize the cunik"""
         # Create the vm with the image
-        self.vm = VM(config)
+        self.id = uuid.uuid4()
+        self.state = 'Not started'
+        vmc = VMConfig()
+        vmc.name = config.name
+        vmc.image_path = config.img
+        vmc.command_line = config.cmd
+        vmc.hypervisor = config.vmm
+        vmc.memory_size = config.mem
+        self.vm = VM(vmc)
         # Register the cunik in the registry
+        # CunikRegistry.register(xxx, self)
 
     def start(self):
         """Start the cunik."""
         # Start the vm
         self.vm.start()
+        self.state = 'Running'
         # Update in registry
+        # CunikRegistry.populate(xxx, self)
 
     def stop(self):
         """Stop the cunik."""
         # Stop the vm
         self.vm.stop()
+        self.state = 'Stopped'
         # Update in registry
+        # CunikRegistry.populate(xxx, self)
 
     def __del__(self):
         """Destroy a cunik according to the config."""
         # Destroy the vm
-        self.vm.destroy()
+        del(self.vm)
         # Remove from registry
+        # CunikRegistry.remove(xxx, self)
+
