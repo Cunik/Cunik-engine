076c2d1a161a0bf0dd8085f4886fe39b02d61e6e
Mingliang Zeng <zengmingliang1998@gmail.com>
2018-06-13 17:06:01 +0800

    Exception handling

--- api/models/cunik/cunik.py
+++ api/models/cunik/cunik.py
@@ -10,32 +10,40 @@ import json
 class CunikConfig:
     """Config of a cunik, constructed when the user wants to create a Cunik."""
     def __init__(self, **kwargs):
-        keys_set = {'name', 'image', 'cmdline', 'hypervisor', 'memory', 'data_volume'}
-        if not set(kwargs.keys()) <= keys_set:
-            raise TypeError(list(set(kwargs.keys()) - keys_set)[0] + ' is an invalid keyword argument for this function')
-        if not kwargs.get('name'):
-            raise KeyError
+        vital_keys_set = {'name', 'image', 'cmdline', 'hypervisor', 'memory', 'data_volume'}
+        all_keys_set = set.union(vital_keys_set, set())
+        if not set(kwargs.keys()) <= all_keys_set:
+            raise KeyError(list(set(kwargs.keys()) - all_keys_set)[0] +
+                           ' is an invalid keyword argument for this function')
+        if not set(kwargs.keys()) >= vital_keys_set:
+            raise KeyError(list(vital_keys_set - set(kwargs.keys()))[0] +
+                           ' is a vital keyword argument for this function but has not been set')
         self.name = kwargs['name']  # name of Cunik instance
         self.image = kwargs['image']  # path to image file
         self.cmdline = kwargs['cmdline']  # command line parameters
         self.hypervisor = kwargs['hypervisor']  # VM type
-        self.memory = kwargs['memory']  # memory size in KB
+        try:
+            self.memory = int(kwargs['memory'])  # memory size in KB
+            if self.memory <= 0:
+                raise ValueError('memory size must be an positive integer')
+        except ValueError:
+            raise ValueError('memory size must be an integer')
         self.data_volume = kwargs['data_volume']  # data volume name
 
     def fill(path_to_cmdline: str, path_to_params: str):
         try:
             with open(path_to_cmdline) as f:
                 cmdline = f.read()
-        except IOError:
-            raise IOError('cmdline file not found')
+        except IOError as IE:
+            raise IOError('cmdline file not found: {0}'.format(IE))
         try:
             with open(path_to_params) as f:
                 try:
                     params = json.loads(f.read())
                 except ValueError as VE:
                     raise ValueError('{0} is not a valid json file: {1}'.format(path_to_params, VE))
-        except IOError:
-            raise IOError('cmdline file not found')
+        except IOError as IE:
+            raise IOError('params file not found: {0}'.format(IE))
         list_of_cmdline = cmdline.split('"')
         list_of_cmdline = [params[p[2:-2]] if p[:2] == '{{' and p[-2:] == '}}' else p for p in list_of_cmdline]
         return '"'.join(list_of_cmdline)
@@ -65,10 +73,7 @@ class Cunik:
         vmc.vdisk_path = config.data_volume
         vmc.hypervisor = config.hypervisor
         vmc.nic = 'tap0'
-        try:
-            vmc.memory_size = int(config.memory)
-        except ValueError:
-            raise ValueError('memory size must be an integer')
+        vmc.memory_size = int(config.memory)
         self.vm = VM(vmc)
         # Register the cunik in the registry
         # CunikRegistry.register(xxx, self)
