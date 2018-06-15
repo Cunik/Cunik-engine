3a85e9a18268fecb57422b88469d7a0556964257
Mingliang Zeng <zengmingliang1998@gmail.com>
2018-06-15 17:27:40 +0800

    Utils update

--- dev/test_scalability.py
+++ dev/test_scalability.py
@@ -12,7 +12,11 @@ image_root = os.path.join(config.cunik_root, 'images/nginx')
 
 T = 200
 
+delay_time = 2
+
 cu = [None] * T
+
+start_time = time.time()
 
 for i in range(1, T + 1):
     os.system('ip l del tap{} 2>/dev/null'.format(i))
@@ -33,20 +37,23 @@ for i in range(1, T + 1):
     cu[i - 1].start()
     print('started cunik{}'.format(i))
 
-time.sleep(2)
+time.sleep(delay_time)
 cnt = 0
 
 for i in range(1, T + 1):
     if not os.system('curl 10.{}.{}.101'.format(120 + i // 100, 120 + i % 100)):
         cnt += 1
 
+end_time = time.time()
+
 if cnt == T:
     print('TEST PASSED!')
 else:
     print('TEST FAILED!')
 
-input('')
+print('Time elapsed: {} sec'.format(end_time - start_time - delay_time))
+
+# input('')
 
 for i in range(1, T + 1):
     cu[i - 1].stop()
-    print('stopped cunik{}'.format(i))
