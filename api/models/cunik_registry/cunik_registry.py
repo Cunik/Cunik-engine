4343f61fe3901714f73262b5965088aef9aa85a8
Mingliang Zeng <zengmingliang1998@gmail.com>
2018-06-22 00:21:00 +0800

    Cunik registry

--- api/models/cunik_registry/cunik_registry.py
+++ api/models/cunik_registry/cunik_registry.py
@@ -1,28 +1,38 @@
 # coding : utf-8
 import json
+import datetime
+import time
+import uuid
 from config import cunik_root
 
 
 class CunikRegistry:
     """Local cunik registry."""
 
-    def __init__(self, Regfile):
-        self._cuniks = json.load(Regfile)
+    def __init__(self, regfile):
+        if regfile:
+            self._cuniks = json.load(regfile)
+        else:
+            self._cuniks = dict()
 
     def register(self, cunik):
         assert not self.query(cunik.id)
+        cunik.create_time = time.time()
         self._cuniks[cunik.id] = cunik
 
-    def query(self, id):
-        return self._cuniks.get(id)
+    def query(self, cid: uuid.UUID):
+        return self._cuniks.get(cid)
 
     def remove(self, cunik):
         assert self.query(cunik.id)
-        self._cuniks[cunik.id].pop(cunik.id)
+        self._cuniks.pop(cunik.id)
 
     def populate(self, cunik):
         assert self.query(cunik.id)
         self._cuniks[cunik.id] = cunik
 
+    def get_id_list(self):
+        return list(self._cuniks.keys())
 
-# cunik_registry = CunikRegistry(Regfile)
+
+cunik_registry = CunikRegistry(None)
