c2ff8ee46afdbf28e6adf0846aa88ca618033c84
shengliangd <shengliangd@outlook.com>
2018-06-23 00:40:21 +0800

    Add redis demo

---
+++ dev/test_redis.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import time
+sys.path.append(os.path.abspath('.'))
+
+from api.models import Cunik, CunikConfig
+
+import config
+
+image_root = os.path.join(config.cunik_root, 'images/redis-server')
+
+os.system('ip l del tap0 2>/dev/null')
+os.system('ip tuntap add tap0 mode tap')
+os.system('ip addr add 10.0.120.100/24 dev tap0')
+os.system('ip link set dev tap0 up')
+
+cfg = CunikConfig(
+    name='cunik0',
+    image=os.path.join(image_root, 'kernel.img'),
+    cmdline=CunikConfig.fill(os.path.join(image_root, 'cmdline'), os.path.join(image_root, 'params.json')),
+    hypervisor='kvm',
+    memory='409600',
+    data_volume=os.path.join(image_root, 'rootfs.iso'),
+)
+
+cu = Cunik(cfg)
+print('waiting')
+cu.start()
+print('started')
+input('Press enter to stop...')
+print('stopped')
