da116e5e6cf11600eb1cf6eb69b9dd79826b794f
Mingliang Zeng <zengmingliang1998@gmail.com>
2018-06-24 00:32:48 +0800

    Add persistency test

--- dev/test_redis.py
+++ dev/test_redis.py
@@ -6,13 +6,24 @@ sys.path.append(os.path.abspath('.'))
 
 from api.models.cunik import CunikApi
 
-CunikApi.create('redis-server', params={'ipv4_addr': '10.0.120.101'})
-print('created')
-time.sleep(2)
-os.system('redis-benchmark --csv -h 10.0.120.101 -c 50 -n 10000 -P 16')
-for i in CunikApi.list():
-    CunikApi.stop(i.id)
-print('stopped')
-for i in CunikApi.list():
-    CunikApi.remove(i.id)
-print('removed')
+L = [1, 2, 3, 4]
+
+if 1 in L:
+    CunikApi.create('redis-server', params={'ipv4_addr': '10.0.120.101'})
+    CunikApi.create('redis-server', params={'ipv4_addr': '10.0.121.101'})
+    print('created')
+    time.sleep(2)
+
+if 2 in L:
+    os.system('redis-benchmark --csv -h 10.0.120.101 -c 50 -n 10000 -P 16')
+    os.system('redis-benchmark --csv -h 10.0.121.101 -c 50 -n 10000 -P 16')
+
+if 3 in L:
+    for i in CunikApi.list():
+        CunikApi.stop(i.id)
+    print('stopped')
+
+if 4 in L:
+    for i in CunikApi.list():
+        CunikApi.remove(i.id)
+    print('removed')
