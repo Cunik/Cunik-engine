da0665039feba43aece7585fb115085f3bc5adb8
hejiyan <hejiyan@mail.ustc.edu.cn>
2018-06-23 17:38:09 +0800

    add log file

--- api/config/base_config.py
+++ api/config/base_config.py
@@ -5,14 +5,14 @@ from logging.handlers import RotatingFileHandler
 
 
 class BaseConfig:
+    CUNIK_ROOT = os.path.realpath('/var/cunik/')
+    LOG_FILE = os.path.realpath('/var/log/cunik.log')
+    REGISTRY_ROOT = os.path.join(CUNIK_ROOT, '/registry/')
+    CUNIK_REGISTRY_FILE = os.path.join(REGISTRY_ROOT, '/cunik.json')
 
     @staticmethod
     def init_app(app):
-        handler = RotatingFileHandler('cunik.log', maxBytes=10000, backupCount=1)
+        handler = RotatingFileHandler(BaseConfig.LOG_FILE, maxBytes=10000, backupCount=1)
         handler.setLevel(logging.WARNING)
         app.logger.addHandler(handler)
-
-    CUNIK_ROOT = os.path.realpath('/var/cunik/')
-    REGISTRY_ROOT = os.path.join(CUNIK_ROOT, '/registry/')
-    CUNIK_REGISTRY_FILE = os.path.join(REGISTRY_ROOT, '/cunik.json')
 
