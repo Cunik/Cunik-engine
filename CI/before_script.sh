05ae9528cd6b858c291b3fa6d90f61fe1b4401ef
Mingliang Zeng <zengmingliang1998@gmail.com>
2018-06-30 05:44:23 +0800

    Update before script

--- CI/before_script.sh
+++ CI/before_script.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-cp config.py.sample config.py
-mkdir .cunik
-cd .cunik
-
-mkdir images
-echo "{}" > images/metadata.json
-
-mkdir volumes
-echo "{}" > volumes/metadata.json
-
-mkdir networkconfigs
-echo "{}" > networkconfigs/metadata.json
+mkdir -p /var/cunik
+cp -R dev/.cunik /var/cunik
